232cf9dda45663220bccbbe0e1deaaaa17f5de3d
Mamykin <mamykin_87@mail.ru>
2023-07-13 15:28:42 +0300

    запуск

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,17 +1,15 @@
-﻿﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// которая выводит третью цифру заданного числа
-// или сообщает, что третьей цифры нет.
+﻿﻿// Задача 13: cd ..
 
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число"); // Выводим сообщение для пользователя
+int number = int.Parse(Console.ReadLine()); // int Числовой считываем данные введеные пользователем
 
-int GetThirdDigit(int number)
+int GetThirdDigit(int number) //заводим пременную
 {
 
     int secondDigit = number % 10;
     return secondDigit;
 }
-if (number < 100)
+if (number < 100)      // проверяет 3 цифру если ее нет.если число меньше 100 третей цифры нет
     Console.WriteLine("Третьей цифры нет");
 else
     Console.WriteLine(GetThirdDigit(number));
